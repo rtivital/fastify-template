0d21ff31625c10ef3433bfd392dc07aff678c203
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-28 16:49:44 +0400

    Add additional server logs

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 const envSchema = z.object({
   PORT: z.preprocess(Number, z.number()).default(4321),
+  HOST: z.string().default('localhost'),
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
 });
 
--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,8 @@ server.get('/err', async (request, reply) => {
 
 const start = async () => {
   try {
-    await server.listen({ port: env.PORT });
+    await server.listen({ port: env.PORT, host: env.HOST });
+    server.log.info(`Server listening at http://${env.HOST}:${env.PORT}`);
   } catch (err) {
     server.log.error(err);
     process.exit(1);
