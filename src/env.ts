3baec37c062d9cb1c3672b2165238f7ae0b08773
Vitaly Rtishchev <rtivital@gmail.com>
2023-09-29 17:20:52 +0400

    Add serve script

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  PORT: z.preprocess(Number, z.number()).default(4321),
+  PORT: z.preprocess(Number, z.number()).default(3000),
   HOST: z.string().default('localhost'),
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
   CORS: z.string().default(''),
